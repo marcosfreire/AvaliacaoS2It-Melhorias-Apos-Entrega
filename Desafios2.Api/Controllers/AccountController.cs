79886de4c9552418c10405cd3d8d92e30797da3d
Marcos Freire <marcos.aurelio.freire@outlook.com>
2018-01-23 13:42:51 -0200

    adicionado account controlller - api project

---
+++ Desafios2.Api/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Desafio.s2.Domain.Core.Interfaces;
+using Desafio.s2.Domain.Core.Notifications;
+using Desafio.s2.Domain.Interfaces;
+using MediatR;
+
+namespace Desafios2.Api.Controllers
+{
+    public class AccountController : BaseController
+    {
+        protected AccountController(INotificationHandler<DomainNotification> notifications,
+                                    IUser user,
+                                    IMediatorHandler mediator) : base(notifications, user, mediator)
+        {
+        }
+    }
+}
