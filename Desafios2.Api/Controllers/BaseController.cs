70491b6081b7d72a749e464982be37098ee42564
Marcos Freire <marcos.aurelio.freire@outlook.com>
2018-01-23 11:10:03 -0200

    adicionado base controller

---
+++ Desafios2.Api/Controllers/BaseController.cs
@@ -0,0 +1,70 @@
+﻿using MediatR;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Desafio.s2.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Desafio.s2.Domain.Core.Interfaces;
+using Desafio.s2.Domain.Core.Notifications;
+
+namespace Desafios2.Api.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        private readonly IMediatorHandler _mediator;
+        private readonly DomainNotificationHandler _notifications;
+
+        protected BaseController(INotificationHandler<DomainNotification> notifications,
+                                 IUser user,
+                                 IMediatorHandler mediator)
+        {
+            _notifications = (DomainNotificationHandler)notifications;
+            _mediator = mediator;
+        }
+
+        protected new IActionResult Response(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notifications.GetNotifications().Select(n => n.Value)
+            });
+        }
+
+        protected bool OperacaoValida()
+        {
+            return (!_notifications.HasNotifications());
+        }
+
+        protected void NotificarErroModelInvalida()
+        {
+            var erros = ModelState.Values.SelectMany(v => v.Errors);
+            foreach (var erro in erros)
+            {
+                var erroMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(string.Empty, erroMsg);
+            }
+        }
+
+        protected void NotificarErro(string codigo, string mensagem)
+        {
+            _mediator.PublicarEvento(new DomainNotification(codigo, mensagem));
+        }
+
+        protected void AdicionarErrosIdentity(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                NotificarErro(result.ToString(), error.Description);
+            }
+        }
+    }
+}
